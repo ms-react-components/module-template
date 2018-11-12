7db69bd9d27184d8b1b0207e5afe1a6adfff06f7
mengshuai <mengshuai@MacBook-Air.local>
2018-11-12 18:29:54 +0800

    修改package，升级依赖

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,15 +1,17 @@
 var path = require("path");
 var webpack = require("webpack");
 var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	entry : './index.js',
-	module : {
-		loaders : [
+	entry: './index.js',
+	module: {
+		loaders: [
 			{
-				loader : 'babel-loader!eslint',
-				test : /\.js$/,
-				exclude : /node_modules/
+				loader: 'babel-loader!eslint',
+				test: /\.js$/,
+				exclude: /node_modules/
 			},
 			{
 				test: /\.scss$/,
@@ -20,32 +22,39 @@ module.exports = {
 				loader: 'file-loader'
 			},
 			{
-		    test: /\.(ttf|eot|svg|woff(2)?)(\?[^('|")]*)?$/, 
-		    loader: "file-loader?name=images/[hash:8].[name].[ext]"
+				test: /\.(ttf|eot|svg|woff(2)?)(\?[^('|")]*)?$/,
+				loader: "file-loader?name=images/[hash:8].[name].[ext]"
 			}
 		]
 	},
 	output: {
 		filename: 'main.bundle.js'
 	},
-	devServer : {
-		filename : 'main.bundle.js',
-		host : '0.0.0.0',
-		port : 3000,
-		disableHostCheck: true
+	devServer: {
+		filename: 'main.bundle.js',
+		host: '0.0.0.0',
+		port: 3000,
+		disableHostCheck: true,
+		quiet: true
 	},
 	eslint: {
-  		configFile: '.eslintrc'
-  		,formatter: require("eslint-friendly-formatter")
+		configFile: '.eslintrc'
+		, formatter: require("eslint-friendly-formatter")
 	},
 	resolve: {
-    modulesDirectories: [
-      'src',
-      'node_modules'
-    ],
-    extensions: ['', '.json', '.js', '.jsx']
+		modulesDirectories: [
+			'src',
+			'node_modules'
+		],
+		extensions: ['', '.json', '.js', '.jsx']
 	},
 	plugins: [
-	    new CaseSensitivePathsPlugin() //检查引用文件名字的大小写
+		new CaseSensitivePathsPlugin() //检查引用文件名字的大小写
+		,new FriendlyErrorsWebpackPlugin({
+			compilationSuccessInfo: {
+				messages: ["The application is running at : http://0.0.0.0:3000/"]
+			}
+		})
+		
 	]
 }
